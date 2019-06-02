f3da5fd528c02727f9627348b8eb4b323a95bffc
Jeyson <darkdream880@gmail.com>
2019-06-01 22:37:07 -0500

    Fin sección 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from 'src/app/services/productos.service';
+import { ProductoDescripcion } from 'src/app/interfaces/producto-descripcion.interface';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  producto: ProductoDescripcion;
+  id: string;
+
+  constructor( private router: ActivatedRoute, public productoService: ProductosService) { }
 
   ngOnInit() {
+    this.router.params.subscribe( parametros => {
+      this.productoService.getProducto(parametros.id)
+        .subscribe( (producto: ProductoDescripcion ) => {
+          this.producto = producto;
+          this.id = parametros.id;
+        });
+    });
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { promise } from 'protractor';
+import { resolve } from 'dns';
+import { reject } from 'q';
 
 
 @Injectable({
@@ -9,19 +12,44 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
 
   cargando = true;
-
   productos: Producto[];
+  productoFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-8ea05.firebaseio.com/productos_idx.json')
-    .subscribe(( res: Producto[]) => {
-      this.productos = res;
-      this.cargando = false;
-      console.log(res);
+
+    // tslint:disable-next-line: no-shadowed-variable
+    return new Promise( ( resolve ) => {
+      this.http.get('https://angular-html-8ea05.firebaseio.com/productos_idx.json')
+      .subscribe(( res: Producto[]) => {
+        this.productos = res;
+        this.cargando = false;
+        resolve();
+      });
     });
+  }
+
+  getProducto(id: string) {
+    return this.http.get(`https://angular-html-8ea05.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto(termino: string) {
+    if ( this.productos ) {
+      this.filtrarProductos( termino );
+    } else {
+      this.cargarProductos().then( () => {
+        this.filtrarProductos( termino );
+      });
+    }
+  }
+
+  filtrarProductos( termino: string ) {
+    this.productoFiltrado = this.productos.filter(
+      producto => producto.categoria.toLowerCase().includes(termino.toLowerCase())
+    );
+    console.log(this.productoFiltrado);
   }
 }
